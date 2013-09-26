a388714e8ce3b01ea787411014e5010dfdc8f41e
Marat Boshernitsan <mboshernitsan@coverity.com>
2013-09-26 15:33:13 -0700

    Stripped down example app to bare minimum

--- src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,13 +1,11 @@
 package com.example.helloworld;
 
-import com.example.helloworld.core.Template;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import io.dropwizard.db.DataSourceFactory;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import com.example.helloworld.core.Template;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class HelloWorldConfiguration extends Configuration {
     @NotEmpty
@@ -15,10 +13,6 @@ public class HelloWorldConfiguration extends Configuration {
 
     @NotEmpty
     private String defaultName = "Stranger";
-
-    @Valid
-    @NotNull
-    private DataSourceFactory database = new DataSourceFactory();
 
     @JsonProperty
     public String getTemplate() {
@@ -42,15 +36,5 @@ public class HelloWorldConfiguration extends Configuration {
 
     public Template buildTemplate() {
         return new Template(template, defaultName);
-    }
-
-    @JsonProperty("database")
-    public DataSourceFactory getDataSourceFactory() {
-        return database;
-    }
-
-    @JsonProperty("database")
-    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
-        this.database = dataSourceFactory;
     }
 }
